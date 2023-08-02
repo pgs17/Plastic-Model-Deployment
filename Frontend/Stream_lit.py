9f4a3261b245d2bb30c38cce82f988b6ac6a18bc
Priyanshu Sarangi <sarangipriyanshu@gmail.com>
2023-08-02 20:52:09 +0530

    Changes

--- Frontend/Stream_lit.py
+++ Frontend/Stream_lit.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(page_title="Plastic Detector")
-st.title("Clean Water Pollution")
+st.title("Object Detection You LOok Once like clown")
 
 image=st.file_uploader("Upload the image in jpg or jpeg",type=['jpg','jpeg'])
 video=st.file_uploader("Upload the video in mp4 format",type=['mp4'])
