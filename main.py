8dca29fe1b5fbea9b06ff7738ef6e9631dcb880f
Priyanshu Sarangi <sarangipriyanshu@gmail.com>
2023-08-03 20:44:10 +0530

    set up post and get functions

---
+++ main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI,HTTPException,UploadFile,File
+import io
+from io import BytesIO
+from ultralytics import YOLO
+import pandas as pd
+import os
+import numpy as np
+import cv2
+from fastapi.responses import RedirectResponse,StreamingResponse
+
+from Helper.helperFunc import add_BoundingBoxes,get_Images_from_Bytes,get_bytes_from_Images,get_model_predict,save_image
+
+
+
+app=FastAPI(title="Detect Plastic")
+
+@app.on_event("startup")
+def startupevent():
+    if not os.path.exists("Prediction"):
+        os.makedirs("Prediction")
+
+    if not os.path.exists("tmp"):
+        os.makedirs("tmp")
+    print("Start Done") 
+
+
+
+@app.get('/')
+def redirect():
+    return RedirectResponse("/docs")
+
+
+@app.post("/prediction_on_image")
+def prediction_on_img(file:UploadFile=File(...)):
+    model=YOLO("Model2(Large).pt")
+    image=get_Images_from_Bytes(file.file.read())
+    predictions=get_model_predict(image,model)
+    print(predictions)
+    bb_box=add_BoundingBoxes(image,predictions)
+    return StreamingResponse(content=get_bytes_from_Images(bb_box),media_type="image/jpg")
+
+
+
+
+
+
+
+
+            
