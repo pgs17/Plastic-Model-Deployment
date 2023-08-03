d4fe77706f5ded9b814b6e7f96d811a5269777dd
Priyanshu Sarangi <sarangipriyanshu@gmail.com>
2023-08-03 21:49:03 +0530

    Endpoints created

--- main.py
+++ main.py
@@ -26,18 +26,30 @@ def startupevent():
 
 
 @app.get('/')
-def redirect():
+async def redirect():
     return RedirectResponse("/docs")
 
 
 @app.post("/prediction_on_image")
-def prediction_on_img(file:UploadFile=File(...)):
+# The File(...) indicates that the parameter is required.
+async def prediction_on_img(file:UploadFile=File(...)):
     model=YOLO("Model2(Large).pt")
     image=get_Images_from_Bytes(file.file.read())
     predictions=get_model_predict(image,model)
     print(predictions)
     bb_box=add_BoundingBoxes(image,predictions)
-    return StreamingResponse(content=get_bytes_from_Images(bb_box),media_type="image/jpg")
+    return StreamingResponse(content=get_bytes_from_Images(bb_box),media_type="image/jpeg")
+
+
+@app.post("/predict_save_image")
+async def predict_and_save(file:UploadFile=File(...)):
+    model=YOLO("Model2(large).pt")
+    img=get_Images_from_Bytes(file.file.read())
+    predictions=get_model_predict(img,model)
+    bb_box=add_BoundingBoxes(img,predictions)
+    processed_image_bytes=get_bytes_from_Images(bb_box)
+    save_image(file_name=file.filename,image=bb_box,prediction=predictions)
+    return StreamingResponse(content=processed_image_bytes,media_type="image/jpeg")
 
 
 
