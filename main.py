48b953c4fa8af07a71307ca2de1b61970fe254ad
Priyanshu Sarangi <sarangipriyanshu@gmail.com>
2023-09-18 14:27:57 +0530

    Added a no of Plastics APi

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 import numpy as np
 import cv2
 from fastapi.responses import RedirectResponse,StreamingResponse
+import json
 
 from Helper.helperFunc import add_BoundingBoxes,get_Images_from_Bytes,get_bytes_from_Images,get_model_predict,save_image
 
@@ -30,18 +31,20 @@ async def redirect():
     return RedirectResponse("/docs")
 
 
-@app.post("/prediction_on_image")
-# The File(...) indicates that the parameter is required.
-async def prediction_on_img(file:UploadFile=File(...)):
-    model=YOLO("Model2(Large).pt")
-    image=get_Images_from_Bytes(file.file.read())
-    predictions,plastic_number=get_model_predict(image,model)
-    print("abc",predictions,plastic_number)
-    print(list(predictions))
-    bb_box=add_BoundingBoxes(image,predictions)
-    return StreamingResponse(content=get_bytes_from_Images(bb_box),media_type="image/jpeg")
 
-
+@app.post("/predict_to_get_plastics")
+async def predict_to_json(file: UploadFile = File(...)):
+    model = YOLO("Model2(Large).pt")
+    image = get_Images_from_Bytes(file.file.read())
+    predictions = get_model_predict(image, model, flag = True)
+    data = json.loads(predictions)
+     
+    info= {
+        "no_of_plastics": len(data),
+        "predictions": data
+    }
+    return info
+   
 @app.post("/predict_save_image")
 async def predict_and_save(file:UploadFile=File(...)):
     model=YOLO("Model2(large).pt")
